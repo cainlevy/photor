18cec30c53a0fae49692ed3bcd7f855291ba7707
Lance Ivy <lance@cainlevy.net>
2012-10-31 23:31:23 -0400

    simplify the import command to a copy command, with less heavyweight dupe checking
    the dupe checking can't be md5- or size-based because those attributes won't
    remain the same when photos are oriented and tagged, and i'm not ready to
    maintain a database of "original" attributes.

--- lib/photor/cli.rb
+++ lib/photor/cli.rb
@@ -3,17 +3,26 @@ require 'thor'
 module Photor
   class CLI < Thor
 
-    desc "import [SOURCE] [DESTINATION]",
-      "copies photos from SOURCE and organizes them by date in DESTINATION"
+    desc "copy [SOURCE] [DESTINATION]",
+      "copies photos from SOURCE into an import folder in DESTINATION"
     long_desc <<-DESC
       Recursively searches the SOURCE directory and all sub-directories for JPEGs.
-      Copies each found JPEG to the DESTINATION directory, organized by YYYY/MM/DD/FILENAME.
-      Will not copy duplicates.
+
+      Copies each found JPEG to an import folder in DESTINATION, which is assumed
+      to be the root of your photo collection.
+
+      Detects and avoids importing duplicates. A duplicate is a JPEG with the same
+      name and date as another that already exists in DESTINATION. No attempt is
+      made to compare using md5 because the existing copy may have been altered
+      by auto-orient, tagging, etc.
     DESC
     method_option :dry_run, :type => :boolean, :desc => "report actions that would be taken without performing them"
-    def import(source, destination)
-      imported = 0
+    def copy(source, destination)
+      copied = 0
       skipped  = 0
+
+      import_folder = File.join(destination, "import #{Time.now.strftime "%Y%m%d-%H%M%S"}")
+      FileUtils.mkdir_p(import_folder) unless options[:dry_run]
 
       puts "scanning:"
       Photor.each_jpeg(source) do |jpg|
@@ -22,31 +31,21 @@ module Photor
         d_path = File.join(destination, jpg.to_path)
 
         if File.exists? d_path
-          existing = Photor::JPEG.new(d_path)
-          if jpg == existing
-            puts "#{d_path} exists" if options[:dry_run]
-            skipped += 1
-            next
-          else
-            i = 0
-            while File.exists? d_path
-              i += 1
-              d_path = d_path.sub(/\.([a-z]*$)/, ".#{i}.\\1")
-            end
-          end
+          puts "#{d_path} exists" if options[:dry_run]
+          skipped += 1
+          next
         end
 
-        imported += 1
+        copied += 1
+
         if options[:dry_run]
-          puts "mkdir -p #{File.dirname(d_path)}"
           puts "cp #{jpg.path} #{d_path}"
         else
-          FileUtils.mkdir_p(File.dirname(d_path))
-          FileUtils.cp jpg.path, d_path
+          FileUtils.cp jpg.path, File.join(import_folder, jpg.name)
         end
       end
       puts "\n"
-      puts "imported: #{imported} skipped: #{skipped}"
+      puts "copied: #{copied} skipped: #{skipped}"
     end
 
     desc "search [SOURCE]",
