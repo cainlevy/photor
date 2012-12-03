891e4fb15c80bac1a1f6e7cb6eb539ed62e562a7
Lance Ivy <lance@cainlevy.net>
2012-12-03 09:22:56 -0500

    `photor organize` will move photos into datetime folders

--- lib/photor/cli.rb
+++ lib/photor/cli.rb
@@ -48,6 +48,28 @@ module Photor
       puts "copied: #{copied} skipped: #{skipped}"
     end
 
+    desc "organize [SOURCE] [DESTINATION]",
+      "organizes photos from SOURCE to DESTINATION by date"
+    long_desc <<-DESC
+      Recursively searches the SOURCE directory and all sub-directories for JPEGs.
+
+      Moves each found JPEG into a date hierarchy in DESTINATION.
+    DESC
+    method_option :dry_run, :type => :boolean, :desc => "report actions that would be taken without performing them"
+    def organize(source, destination)
+      Photor.each_jpeg(source) do |jpg|
+        print "."
+
+        d_path = File.join(destination, jpg.to_path)
+        if options[:dry_run]
+          puts "moving #{jpg.path} to #{d_path}"
+        else
+          FileUtils.mkdir_p File.dirname(d_path)
+          FileUtils.mv jpg.path, d_path
+        end
+      end
+    end
+
     desc "orient [FOLDER]",
       "auto-orients photos in FOLDER"
     long_desc <<-DESC
