c1e71d9f88f6e29768d9435d98209f07d5ef1338
Lance Ivy <lance@cainlevy.net>
2013-02-18 22:08:38 -0500

    utility to rename files into unique name format (time+md5)
    this isn't perfect because the original md5 can't be recreated. but
    it's consistent and still unguessable.

---
+++ lib/photor/cli/rename.rb
@@ -0,0 +1,34 @@
+require 'thor'
+
+class Photor::CLI < Thor
+  desc "rename [FOLDER]",
+    "renames all photos in FOLDER with unique names"
+  long_desc <<-DESC
+    Renames every JPG in FOLDER with the unique name pattern
+    employed by the `copy' command. Meant to convert an existing
+    gallery to the new naming convention.
+  DESC
+  method_option :dry_run, :type => :boolean, :desc => "report actions that would be taken without performing them"
+  def rename(folder)
+    renamed = 0
+    skipped = 0
+
+    puts "scanning:"
+    Photor.each_jpeg(folder) do |jpg|
+      print "."
+      if jpg.name.match(/^\d{6}-[0-9a-f]{32}\.[a-z]*$/)
+        skipped += 1
+      else
+        renamed += 1
+        new_path = jpg.path.sub(jpg.name, jpg.unique_name)
+        if options[:dry_run]
+          puts "#{jpg.path} -> #{new_path}"
+        else
+          FileUtils.mv(jpg.path, new_path)
+        end
+      end
+    end
+    puts "\n"
+    puts "renamed: #{renamed} skipped #{skipped}"
+  end
+end
