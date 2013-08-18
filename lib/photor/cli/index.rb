60e38ec7a3a63f76f9b37ad5671c6ff8d85660c2
Lance Ivy <lance@cainlevy.net>
2013-08-18 10:34:34 -0400

    make `photor index` and `photor search` use CWD by default

--- lib/photor/cli/index.rb
+++ lib/photor/cli/index.rb
@@ -5,22 +5,22 @@ require_relative '../db/photo'
 
 class Photor::CLI < Thor
 
-  desc "index [DESTINATION]",
-    "indexes metadata about photos in DESTINATION"
+  desc "index",
+    "indexes photo metadata into a read-only database cache"
   long_desc <<-DESC
-    Recursively searches DESTINATION directory for all JPEGs, and records metadata
-    useful for future searching, tagging, and de-duping.
+    Recursively searches for all JPEGs, and records metadata useful for future
+    searching, tagging, and de-duping.
 
-    All metadata is stored in a SQLite database in the DESTINATION's root. This
-    means the db can be archived and accessed with the photos, by multiple clients
-    (but not concurrently).
+    All metadata is stored in a SQLite database. This means the db can be archived
+    and accessed with the photos, by multiple clients (but not concurrently).
 
-    When possible, the database should strive to be an index of data that already
-    exists in photo headers (e.g. EXIF).
+    The database only caches metadata from each photo, to speed up certain
+    operations. The photo headers (e.g. EXIF) are still the canonical data source.
   DESC
-  def index(destination)
-    db = Photor::DB.new(destination)
-    Photor.each_jpeg(destination) do |jpg|
+  method_option :dir, :type => :string, :default => '.', :desc => "location of photos and sqlite3 db"
+  def index
+    db = Photor::DB.new(options[:dir])
+    Photor.each_jpeg(options[:dir]) do |jpg|
       print "."
 
       photo = db.photos.find_or_create(jpg.name, jpg.taken_at)
