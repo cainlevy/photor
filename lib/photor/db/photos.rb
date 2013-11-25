89340d7f9b0879a2bbee7fa5ba0c2d82b78cf2c5
Lance Ivy <lance@cainlevy.net>
2013-11-24 19:35:35 -0500

    generate some sql

--- lib/photor/db/photos.rb
+++ lib/photor/db/photos.rb
@@ -2,22 +2,32 @@ require_relative '../db'
 
 class Photor::DB
   class Photos < Table
+    NAME = 'photos'
+    COLUMNS = %w(id filename taken_at)
+
     def find(filename, taken_at)
-      select(Photo, "SELECT id, filename, taken_at FROM photos WHERE filename = ? AND taken_at = ?", filename, taken_at.to_i)
+      select(Photo, "SELECT #{COLUMNS.join(', ')} FROM #{NAME} WHERE filename = ? AND taken_at = ?", filename, taken_at.to_i)
     end
 
     def find_by_tag(tag)
       select_all(Photo, <<-SQL, tag)
-        SELECT photos.id, photos.filename, photos.taken_at
-        FROM photos
+        SELECT #{COLUMNS.map{|c| [NAME, c].join('.') }.join(', ')}
+        FROM #{NAME}
         INNER JOIN taggings ON taggings.photo_id = photos.id
         INNER JOIN tags ON taggings.tag_id = tags.id
         WHERE tags.name = ?
       SQL
     end
 
+    def years
+      db.execute(<<-SQL).flatten.sort
+        SELECT DISTINCT YEAR(taken_at)
+        FROM #{NAME}
+      SQL
+    end
+
     def create(filename, taken_at)
-      db.execute("INSERT INTO photos (id, filename, taken_at) VALUES (NULL, ?, ?)", filename, taken_at.to_i)
+      db.execute("INSERT INTO #{NAME} (#{COLUMNS.join(', ')}) VALUES (NULL, ?, ?)", filename, taken_at.to_i)
       Photo.new(db, {'id' => db.last_insert_row_id, 'filename' => filename, 'taken_at' => taken_at.to_i})
     end
 
